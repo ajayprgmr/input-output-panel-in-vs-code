685505b61c99da9bef8d6554f233f47659aef692
Ajay Kushwaha <86186444+ajayprgmr@users.noreply.github.com>
2023-02-15 09:59:19 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,1 +1,10 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+ifstream input("input.txt");  // here input.txt create in the same folder or we can give proper path also
+ofstream output("output.txt");   // here outut.txt create in the same folder or we can give proper path also
+#define cin input
+#define cout output 
+
+// include this code in your code and split your screen into three parts. better be go to this url of you tube you will understand everything
+      
